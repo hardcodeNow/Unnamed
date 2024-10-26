2fa6cb265442e2a56a78be5cd256e6fffaf424d6
AirboZH <i@airbozh.cn>
2024-10-26 15:44:02 +0800

    feat: add backend trpc

--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -1,95 +1,111 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { z } from "zod";
 
-const postSchema = z.object({
+const cardSchema = z.object({
   id: z.number().optional(),
-  name: z.string(),
-  outline: z.string(),
+  content: z.string(),
+  postId: z.number(),
   createdAt: z.date().optional(),
   updatedAt: z.date().optional(),
 });
 
-const createPost = publicProcedure
+const createCard = publicProcedure
   .input(
-    postSchema.omit({
+    cardSchema.omit({
       id: true,
       createdAt: true,
       updatedAt: true,
     })
   )
   .mutation(async ({ input, ctx }) => {
-    return ctx.db.post.create({
+    return ctx.db.card.create({
       data: {
         ...input,
       },
     });
   });
 
-const updatePost = publicProcedure
+const readCard = publicProcedure
+  .input(z.number())
+  .query(async ({ input, ctx }) => {
+    return ctx.db.card.findUnique({
+      where: { id: input },
+      include: {
+        post: true,
+      },
+    });
+  });
+
+const updateCard = publicProcedure
   .input(
-    postSchema.omit({
+    cardSchema.omit({
       createdAt: true,
       updatedAt: true,
     })
   )
   .mutation(async ({ input, ctx }) => {
     const { id, ...data } = input;
-    return ctx.db.post.update({
+    return ctx.db.card.update({
       where: { id },
       data,
     });
   });
 
-const deletePost = publicProcedure
+const deleteCard = publicProcedure
   .input(z.number())
   .mutation(async ({ input, ctx }) => {
-    return ctx.db.post.delete({
+    return ctx.db.card.delete({
       where: { id: input },
     });
   });
 
-const listPosts = publicProcedure
+const listCards = publicProcedure
   .input(
     z.object({
       limit: z.number().min(1).max(100).optional(),
       cursor: z.number().optional(),
+      postId: z.number().optional(),
     }).optional()
   )
   .query(async ({ input, ctx }) => {
     const limit = input?.limit ?? 50;
     const cursor = input?.cursor;
 
-    return ctx.db.post.findMany({
+    return ctx.db.card.findMany({
       take: limit + 1,
       cursor: cursor ? { id: cursor } : undefined,
+      where: input?.postId ? {
+        postId: input.postId
+      } : undefined,
       orderBy: {
         createdAt: "desc",
       },
       include: {
-        card: true,
+        post: true,
       },
     });
   });
 
-const searchPosts = publicProcedure
+const searchCards = publicProcedure
   .input(z.string())
   .query(async ({ input, ctx }) => {
-    return ctx.db.post.findMany({
+    return ctx.db.card.findMany({
       where: {
-        name: {
+        content: {
           contains: input,
         },
       },
       include: {
-        card: true,
+        post: true,
       },
     });
   });
 
-export const postRouter = createTRPCRouter({
-  create: createPost,
-  update: updatePost,
-  delete: deletePost,
-  list: listPosts,
-  search: searchPosts,
+export const cardRouter = createTRPCRouter({
+  create: createCard,
+  read: readCard,
+  update: updateCard,
+  delete: deleteCard,
+  list: listCards,
+  search: searchCards,
 });
